4c66f0aea34c48ef0f6d86214511ce0eb1fed084
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2026-02-07 13:13:16 +0100

    added uuid to task and todolist

--- src/app/todo.js
+++ src/app/todo.js
@@ -1,11 +1,16 @@
+import{v4 as uuidv4} from 'uuid';
+
 function todolist(name){
+    const id = uuidv4();
     return {
+        id,
         name,
         taskList:[]
     };
 };
 
 function task(title, description, dueDate, priority, notes, checklist, status){
+    const id = uuidv4();
 
 
     return {
@@ -15,7 +20,8 @@ function task(title, description, dueDate, priority, notes, checklist, status){
         priority, 
         notes, 
         checklist, 
-        status
+        status,
+        id
     };
 };
 
